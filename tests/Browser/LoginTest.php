46440c0203dd04487ba60e631a7bac63c6012db1
JIasheng Tang <s3598097@student.rmit.edu.au>
2019-05-08 23:41:46 +1000

    Travis test8

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -11,14 +11,7 @@ class LoginTest extends DuskTestCase
     public function testExample()
     {
         $this->browse(function (Browser $browser) {
-            $browser->visit('/register')
-                ->type('email', 'test@test.com')
-                ->type('name', 'edgar')
-                ->type('mobile', '0400000000')
-                ->type('password', '123')
-                ->type('confirm-password', '123')
-                ->click('#register-btn')
-                ->visit('/login')
+            $browser->visit('/login')
                 ->type('email', 'test@test.com')
                 ->type('password', '123')
                 ->click('#m_login_signin_submit')
